b7f5d64922fd0bde90683dc29db84b1cbb187cf4
zackcarson <zack.carson@virginia.edu>
2019-03-17 17:38:01 -0400

    adding Hectors references

--- paper/go.sh
+++ paper/go.sh
@@ -9,6 +9,7 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> out
 pdflatex --shell-escape bin-love-after-gw170817.tex >> output.text
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> output.text
 pdflatex --shell-escape bin-love-after-gw170817.tex >> output.text
-#rm *blg *log *aux *Notes* *bbl output.text *out
+rm *blg *log *aux *Notes*  output.text *out
+mv bin-love-after-gw170817.pdf ../
 echo "Done. Enjoy your PDF!"
-evince bin-love-after-gw170817.pdf &
+evince ../bin-love-after-gw170817.pdf &
